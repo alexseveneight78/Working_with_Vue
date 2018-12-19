d218ac0cad5f8f684d0e156b845fdbbe9ad3c84b
Alex <37299635+alexseveneight78@users.noreply.github.com>
2018-12-19 18:10:02 +0300

    Initial commit

---
+++ components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>The name of header is : {{ header }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      header: "Working with assembling Vue Components"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  width: 30%;
+  background-color: lightgreen;
+  padding: 5px 20px;
+  border: 1px dashed green;
+  border-radius: 0% 75% 0% 75%;
+}
+</style>
---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Here is {{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      text: "Change background"
+    };
+  }
+};
+</script>
